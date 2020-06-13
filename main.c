bad9635dd52869fe3192c37cd3d8a9ee2700a927
Vishwa Shankar <vishwas1020@gmail.com>
2020-06-13 15:48:45 -0700

    btree insert and delete btree.h due to include errors

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "buffer.h"
 #include "memory.h"
+
 
 #define STATEMENT_SUCCESS 0
 #define STATEMENT_FAILURE -1
@@ -19,6 +22,8 @@ extern const uint32_t ROWS_PER_PAGE;
 extern const uint32_t MAX_ROWS;
 extern const uint32_t COL_1_OFFSET;
 extern const uint32_t COL_2_OFFSET;
+extern const uint32_t LEAF_MAX_CELLS;
+extern const uint32_t LEAF_NODE_CELL_SIZE;
 
 typedef enum{
     INSERT_STATEMENT,
@@ -107,14 +112,34 @@ void deserialize_data(void* src_row, Row* dest_row){
     memcpy(&(dest_row->col2),src_row+COL_2_OFFSET,COL_2_SIZE);
 }   
 
+void leaf_node_insert(Cursor* cursor, uint32_t key, Row* row){
+    void* node = get_page_data(cursor->table->file_pager,cursor->page_number);
+    uint32_t num_cells = *leaf_node_number_cells(node);
+    if(num_cells>LEAF_MAX_CELLS){
+        printf("Need to implement splitting a leaf node.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(cursor->cell_number<num_cells){
+        for(int i=num_cells; i> cursor->cell_number;i--){
+            memcpy(get_leaf_node_cell(node,i),get_leaf_node_cell(node,i-1),LEAF_NODE_CELL_SIZE);
+        }
+    }
+    *(leaf_node_number_cells(node))+=1;
+    *(get_leaf_key(node,cursor->cell_number)) = key;
+    serialize_data(row,get_leaf_node_value(node,cursor->cell_number));
+
+}
+
 int execute_insert(Statement* statement, Table* table){
     Cursor* cursor = init_end_cursor(table);
     //check if table is full
-    if(table->number_of_rows==MAX_ROWS){
+    void* node = get_page_data(table->file_pager, table->root_page);
+    if(*leaf_node_number_cells(node) >= LEAF_MAX_CELLS){
         return STATEMENT_FAILURE;
     }
-    serialize_data(&(statement->insert_row),get_cursor_page_data(cursor));
-    table->number_of_rows+=1;
+    Row* row = &(statement->insert_row);
+    leaf_node_insert(cursor, row->row_id, row);
     return STATEMENT_SUCCESS;
 
 }
@@ -123,7 +148,7 @@ int execute_select(Statement* statement, Table* table){
     Cursor* cursor = init_start_cursor(table);
     Row ret_row;
     while(!cursor->is_end){
-        deserialize_data(get_cursor_page_data(cursor),&ret_row);
+        deserialize_data(get_cursor_value(cursor),&ret_row);
         printf("%d| %s| %s\n",ret_row.row_id,ret_row.col1,ret_row.col2);
         increment_cursor(cursor);
     }
@@ -134,6 +159,13 @@ int execute_select(Statement* statement, Table* table){
 int main(int argc, char* argv[]){
     print_help_info();
     InputBuffer* input_buffer = new_input_buffer();
+
+    if (mkdir("db_files", 0777) == -1) {
+        printf("ERROR creating directory 'db_files', db_files already exists\n");
+    }
+    else{
+        printf("db_files does not exist. Directory created\n");
+    }
     Table* sample_table = init_sqlyt_db(argv[1]);
     printf("Opening file: db_files/%s\n",argv[1]);
     while(true){
--- memory.h
+++ memory.h
@@ -8,7 +8,7 @@
 
 typedef struct Row{
     int row_id;
-    char col1[SAMPLE_COLUMN_SIZE_1]; //currently test table with only 3 columns, future plans to expand to more columns
+    char col1[SAMPLE_COLUMN_SIZE_1]; //currently test table with only 3 columns, future plans to expand to more columns based on args from insert or adding a new create table statement?
     char col2[SAMPLE_COLUMN_SIZE_2];
 } Row;
 
@@ -16,17 +16,20 @@ typedef struct Row{
 typedef struct Pager{
     void* pages[MAX_PAGES_PER_TABLE];
     int fd;
+    int num_pages;
     int flength;
 } Pager;
 
 typedef struct Table{
     int number_of_rows;
+    int root_page;
     Pager* file_pager;
 } Table;
 
 
 typedef struct Cursor{
-    int row_number;
+    int page_number;
+    int cell_number;
     Table* table;
     bool is_end;
 } Cursor;
@@ -42,11 +45,31 @@ void increment_cursor(Cursor* cursor);
 
 void* get_page_data(Pager* pager,int page_number);
 
-int flush_page_data(Pager* pager, int page_number,int size);
+int flush_page_data(Pager* pager, int page_number);
 
 void close_sqlyt_db(Table* table);
 
-void* get_cursor_page_data(Cursor* cursor);
+void* get_cursor_value(Cursor* cursor);
 
 void free_pager_table(Table* table);
+
+
+typedef enum {
+    INTERNAL_NODE,
+    LEAF_NODE
+} NodeType;
+
+
+uint32_t* leaf_node_number_cells(void* node);
+
+void init_leaf(void* node);
+
+void* get_leaf_node_cell(void* node,uint32_t cell_number );
+
+uint32_t* get_leaf_key(void* node,uint32_t cell_number);
+
+void* get_leaf_node_value(void* node, uint32_t cell_number);
+
+
+
 #endif
